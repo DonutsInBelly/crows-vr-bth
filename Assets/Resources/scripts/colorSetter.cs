272891773aa1f5f7d9312d26612538f6bc9eed09
Carlin Au <carlinau@gmail.com>
2016-06-19 10:21:26 -0400

    colorSetter script

---
+++ Assets/Resources/scripts/colorSetter.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System;
+using System.Net;
+using System.Collections;
+
+public class colorSetter : MonoBehaviour {
+
+	string URL = "http://223bce18.ngrok.io/cs";
+	string JSON;
+
+    void Colorize()
+    {
+        InvokeRepeating("GetRequest", 1, 1f);
+    }
+
+	// Use this for initialization
+	void Start () {
+        print("Hi!");
+        Colorize();        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Console.Write("Hi!!");
+    }
+
+	void FixedUpdate() {
+	}
+
+	void GetRequest (){
+		print ("Doing the thing!");
+		using (WebClient webClient = new WebClient ()) {
+			JSON = webClient.DownloadString (URL);
+			print (JSON);
+		}
+	}
+}
