47674baf929867c57e483ff54537a3711aa47829
PJ Rosa <demotednyk@gmail.com>
2016-06-18 21:37:55 -0400

    pj's first commit

---
+++ Crows/Assets/scripts/shader_mut.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using System;
+using System.Collections.Generic;
+
+public class shader_mut : MonoBehaviour {
+
+	// Use this for initialization
+	public Renderer albedo;
+	public String mood;
+	public var argList;
+	void Start () {
+		argList = new List<KeyValuePair<string, Color>>();
+		albedo = GetComponent<Renderer>();
+		argList.Add(new KeyValuePair<string, Color>("sad", Color.blue));
+		argList.Add(new KeyValuePair<string, Color>("happy", Color.yellow));
+		argList.Add(new KeyValuePair<string, Color>("angry", Color.red));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for (int i = 0; i <= argList.Length(); i++) {
+			
+		}
+	}
+}
